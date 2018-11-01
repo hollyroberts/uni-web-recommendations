a83943697977da8b73e95c434b3f7c2858cc288b
blackenedgem <Chrisroberts97@btinternet.com>
2018-11-01 22:26:49 +0000

    Split into base, add bootstrap

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    print("TEST")
     return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.add_url_rule('/favicon.ico',
+                     redirect_to=url_for('static', filename='favicon.ico'))
     app.run()
