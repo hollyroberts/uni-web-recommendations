f4089fa5ac522d39ec5a52f8bac4ba2419fe0602
blackenedgem <Chrisroberts97@btinternet.com>
2018-11-09 22:00:19 +0000

    Finalise convert db script

--- convert_db.py
+++ convert_db.py
@@ -1,9 +1,11 @@
 # Standalone program to setup SQLite db
-# Converts CSV files INTEGERo tables
+# Converts CSV files into tables
+# This takes a fair chunk of memory (~5-6GB), and takes about a minute or so to run
 
 import sqlite3
 import os
 import csv
+import sys
 
 MVLENS_FOLDER = "data"
 
@@ -11,34 +13,78 @@ os.remove("database.db")
 db = sqlite3.connect("database.db")
 
 # Create tables
-db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY , name Varchar)")
-db.execute("CREATE TABLE movies (id INTEGER PRIMARY KEY , title Varchar)")
-db.execute("CREATE TABLE genres (id INTEGER PRIMARY KEY , genre Varchar)")
+db.execute("CREATE TABLE users (id INTEGER PRIMARY KEY , name VARCHAR, from_mvl BOOLEAN)")
+db.execute("CREATE TABLE movies (id INTEGER PRIMARY KEY , title VARCHAR)")
+db.execute("CREATE TABLE genres (id INTEGER PRIMARY KEY , genre VARCHAR)")
 
-db.execute("CREATE TABLE ratings (user_id INTEGER, movie_id INTEGER, rating_score INTEGER)")
 db.execute("CREATE TABLE movie_genres (movie_id INTEGER, genre_id INTEGER)")
+db.execute("CREATE TABLE ratings (user_id INTEGER, movie_id INTEGER, rating_score FLOAT)")
 
 # Go through movies.csv
 with open(os.path.join(MVLENS_FOLDER, "movies.csv"), 'r', encoding="UTF-8") as file:
-    genre_set = set()
-    to_movie_db = []
+    genre_dict = {}
+    number_of_genres = 0
+
+    movie_data = []
+    movie_genre_data = []
 
     data = csv.DictReader(file)
 
     for row in data:
-        to_movie_db.append((row['movieId'], row['title']))
-        genre_set.update(row['genres'].split('|'))
+        movie_data.append((row['movieId'], row['title']))
 
-    genre_set.remove("(no genres listed)")
+        genres_in_movie = row['genres'].split('|')
+        if "(no genres listed)" in genres_in_movie:
+            genres_in_movie.remove("(no genres listed)")
 
-# Post process genres
-genre_tuples = list((i,) for i in sorted(genre_set))
-genre_map = {value[0]: index for (index, value) in enumerate(genre_tuples)}
+        for genre in genres_in_movie:
+            # Add to dictionary if genre doesn't exist
+            if genre not in genre_dict:
+                number_of_genres += 1
+                genre_dict[genre] = number_of_genres
 
-# Add data to movies and genres
-db.executemany("INSERT INTO movies (id, title) VALUES (?, ?)", to_movie_db)
-db.executemany("INSERT INTO genres (genre) VALUES (?)", genre_tuples)
-print(db.execute("SELECT * FROM genres").fetchall())
+            # Add to obj
+            movie_genre_data.append((row['movieId'], genre_dict[genre]))
+
+# Invert genres
+genre_data = list((index, genre) for (genre, index) in genre_dict.items())
+
+# Add data to movies, genres, and movie genres
+db.executemany("INSERT INTO movies (id, title) VALUES (?, ?)", movie_data)
+db.executemany("INSERT INTO genres (id, genre) VALUES (?, ?)", genre_data)
+db.executemany("INSERT INTO movie_genres (movie_id, genre_id) VALUES (?, ?)", movie_genre_data)
+
+# Go through ratings.csv
+with open(os.path.join(MVLENS_FOLDER, "ratings.csv"), 'r', encoding="UTF-8") as file:
+    users = [1]
+    max_user = 0
+
+    rating_data = []
+
+    data = csv.DictReader(file)
+
+    for row in data:
+        rating_data.append((row['userId'], row['movieId'], row['rating']))
+
+        max_user = max(max_user, int(row['userId']))
+        if max_user > users[-1]:
+            users.append(max_user)
+
+# Create dummy users and ensure users increase constantly
+user_data = []
+
+for index, user_id in enumerate(users):
+    if index + 1 != user_id:
+        print(f"Error on ensuring user continuity: {user_id}")
+        sys.exit(-1)
+
+    user_data.append((user_id, user_id, True))
+print(f"Number of users: {max_user}")
+
+# Fill in users and ratings
+db.executemany("INSERT INTO users (id, name, from_mvl) VALUES (?, ?, ?)", user_data)
+db.executemany("INSERT INTO ratings (user_id, movie_id, rating_score) VALUES (?, ?, ?)", rating_data)
 
 db.commit()
 db.close()
+
