d2189465da2e633577f46b3e0a0bc4eb1209b9ea
blackenedgem <Chrisroberts97@btinternet.com>
2018-11-19 21:38:48 +0000

    Basic matrix stuff

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import pandas
+import pandas as pd
+import numpy as np
+from scipy.sparse.linalg import svds
+
 import sqlite3, time
 from flask import Flask, session, render_template, redirect, request, jsonify
 
@@ -8,18 +11,35 @@ app = Flask(__name__, static_url_path='/static')
 
 start = time.time()
 with sqlite3.connect("database.db") as db:
-    print("Loading movies")
-    movies = pandas.read_sql("SELECT id, title FROM movies", db)
-    print(f"Loaded movies in {round(time.time() - start, 1)} seconds")
+    # Get movies from SQL query
+    merged_data = pd.read_sql("SELECT user_id, movie_id, rating_score FROM ratings", db)
 
-    print("Loading ratings")
-    print("This will take a while (about a minute) and use a fair amount of memory (~3GB) while loading the database")
+    # Setup data frames
+    ratings_mean_count = pd.DataFrame(merged_data.groupby('movie_id')['rating_score'].mean())
+    ratings_mean_count['rating_counts'] = pd.DataFrame(merged_data.groupby('movie_id')['rating_score'].count())
+
+    R_df = merged_data.pivot(index='user_id', columns='movie_id', values='rating_score').fillna(0)
+    R = R_df.values
+
+    # Decompose
+    user_ratings_mean = np.mean(R, axis=1)
+    R_demeaned = R - user_ratings_mean.reshape(-1, 1)
+    U, sigma, Vt = svds(R_demeaned, k=3)
+
+    predicted_ratings = np.dot(np.dot(U, np.diag(sigma)), Vt) + user_ratings_mean.reshape(-1, 1)
+    preds_df = pd.DataFrame(predicted_ratings, columns=R_df.columns)
+
+    print(preds_df.head())
+
+    # print("Loading movies")
+    # movies = pandas.read_sql("SELECT id, title FROM movies", db)
+    # print(f"Loaded movies in {round(time.time() - start, 1)} seconds")
+    #
+    # print("Loading ratings")
+    # print("This will take a while (about a minute) and use a fair amount of memory (~3GB) while loading the database")
     # ratings = pandas.read_sql("SELECT user_id, movie_id, rating_score FROM ratings", db)
-    print(f"Loaded ratings in {round(time.time() - start, 1)} seconds")
-    print("Done")
-
-print(movies.head())
-# print(ratings.head())
+    # print(f"Loaded ratings in {round(time.time() - start, 1)} seconds")
+    # print("Done")
 
 @app.route('/')
 @app.route('/index.html')
